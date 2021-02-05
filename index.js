cb7ec071d0479213ce7e41fe621ad1778db47762
yckj3348 <cuijinxin@cloudwalk.com>
2021-02-05 10:16:44 +0800

    界面调整

--- index.js
+++ index.js
@@ -4,19 +4,26 @@
  * @Autor: cuijinxin
  * @Date: 2021-02-03 15:25:58
  * @LastEditors: cuijinxin
- * @LastEditTime: 2021-02-03 20:45:15
+ * @LastEditTime: 2021-02-04 17:02:28
  */
-// 161725;501205;001500;004854;160629;010955
+// 161725|501205|001500|004854|160629|010955
+
+// 变量定义
+let step = 0; // 0 列表页 1 设置页
+
 // 封装http请求
-let getFundData = (fundCode, resolve) => {
-  console.log(fundCode)
+let getFundData = (fundCode, resolve, reject) => {
   let xhr = new XMLHttpRequest();
   xhr.onreadystatechange = (res) => {
-    if(res.target.readyState === 4 && res.target.response){
-      let json = JSON.parse( `{
-        ${res.target.response.split("({")[1].split("})")[0]}
-      }` );
-      resolve(json)
+    try {
+      if(res.target.readyState === 4 && res.target.response){
+        let json = JSON.parse( `{
+          ${res.target.response.split("({")[1].split("})")[0]}
+        }` );
+        resolve(json)
+      }
+    } catch {
+      reject()
     }
   };
   xhr.open("GET", `http://fundgz.1234567.com.cn//js/${fundCode}.js?rt=${(new Date()).getTime()}`, true);
@@ -25,11 +32,11 @@ let getFundData = (fundCode, resolve) => {
 
 // 获取数据
 let searchData = (fundList) => {
-  let fund = fundList.split(";")
+  let fund = fundList.split("|")
   let request = [];
   for(let i = 0; i < fund.length; i++) {
-    request.push( new Promise((resolve) => {
-      getFundData(fund[i], resolve)
+    request.push( new Promise((resolve, reject) => {
+      getFundData(fund[i], resolve, reject)
     }))
   }
   Promise.all(request).then( res => {
@@ -39,35 +46,93 @@ let searchData = (fundList) => {
     })
     initfundHTML(fundData)
     console.log(fundData)
+  }).catch( () => {
+    console.log("error")
   })
 }
 // 渲染dom
 let initfundHTML = (data) => {
   let content = document.getElementById("fundInfoList");
-  content.innerHTML = ""; //清空
   let len = Object.keys(data).length;
-  let ele = ""
+  let ele = '<div class="header">\
+              <span class="headerL">长势</span>\
+              <span class="headerM">韭菜品种</span>\
+              <span class="headerR">更新时间</span>\
+            </div>'
   for(let i = 0; i < len; i++){
     let info = Object.values(data)[i];
     ele += '<div class="content"> \
       <img class="icon" src="./static/' + (info.gszzl > 0 ? "up" : "down") +'.png" ></img> \
       <span class="precent" style="color:' + (info.gszzl > 0 ? "red" : "green") +'">' + info.gszzl +'%</span> \
-      <span class="fundName">' + info.name + '</span> \
-      <span class="updateTime">' + info.gztime.split(" ")[0] + '</span> \
+      <span class="fundName">' + `${info.name} [${info.fundcode}]` + '</span> \
+      <span class="updateTime">' + info.gztime.split(" ")[1] + '</span> \
     </div>'
   }
   content.innerHTML = ele;
 }
-(function() {
-  chrome.storage.sync.get("fundCode", (res) => {
-    document.getElementById("fundList").value = res.fundCode;
-    searchData(res.fundCode)
-  })
+// 页面切换
+let changeCurPage = (step) => {
+  let fundInfoContent = document.getElementById("fundInfoContent");
+  let fundSetting = document.getElementById("fundSetting");
+  if(step === 0) { // 列表页
+    fundInfoContent.style.display = "block";
+    fundSetting.style.display = "none";
+    chrome.storage.sync.get("fundCode", (res) => {
+      document.getElementById("fundList").value = res.fundCode || "";
+      if(res.fundCode){
+        searchData(res.fundCode)
+        document.getElementById("errorTips").style.display = "none";
+      } else {
+        document.getElementById("fundInfoList").innerHTML = "";
+        document.getElementById("errorTips").style.display = "block";
+      }
+    })
+  } else if( step === 1) { // 配置页
+    fundInfoContent.style.display = "none";
+    fundSetting.style.display = "block";
+  }
+}
+// 初始化时绑定相关事件
+let bindEvent = () => {
+  // 
   document.getElementById("setFund").onclick = () => {
     chrome.storage.sync.set(
       { "fundCode": document.getElementById("fundList").value }, () => {
-        console.log("保存成功")
+        document.getElementById("setFundTips").style.opacity = 1;
+        setTimeout( () => {
+          document.getElementById("setFundTips").style.opacity = 0;
+        }, 1000)
       }
     );
   }
+  // 切换页面
+  document.getElementById("stepLeft").onclick = () => {
+    step -= 1
+    step = step < 0 ? 1 : step
+    changeCurPage(step)
+  }
+  document.getElementById("stepRight").onclick = () => {
+    step += 1
+    step = step > 1 ? 0 : step
+    changeCurPage(step)
+  }
+  // 跳转到设置页
+  document.getElementById("goSetting").onclick = () => {
+    step = 1
+    changeCurPage(step)
+  }
+}
+
+(function() {
+  chrome.storage.sync.get("fundCode", (res) => {
+    document.getElementById("fundList").value = res.fundCode || "";
+    if(res.fundCode){
+      searchData(res.fundCode)
+      document.getElementById("errorTips").style.display = "none";
+    } else {
+      document.getElementById("fundInfoList").innerHTML = "";
+      document.getElementById("errorTips").style.display = "block";
+    }
+  })
+  bindEvent()
 })();
